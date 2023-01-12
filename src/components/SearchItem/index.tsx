9292c8a2347f3ee97aec074ff6a8b77953d0c8ab
Young il Cho <0young@kakao.com>
2023-01-12 13:51:08 +0900

    fix: 하이라이트 오류 수정

--- src/components/SearchItem/index.tsx
+++ src/components/SearchItem/index.tsx
@@ -8,16 +8,16 @@ const SearchItem = ({ sickCd, sickNm, value }: Props) => {
   const content = useMemo(() => {
     const _cutted = sickNm.split(value);
     return _cutted.map((ele, idx) => {
-      if (ele === "")
-        return (
-          <div key={sickCd + ele}>
-            <Styled.Strong>{value}</Styled.Strong>
-          </div>
-        );
       if (idx === _cutted.length - 1)
         return (
           <div key={sickCd + ele}>
             <p>{ele}</p>
+          </div>
+        );
+      if (ele === "")
+        return (
+          <div key={sickCd + ele}>
+            <Styled.Strong>{value}</Styled.Strong>
           </div>
         );
       return (
