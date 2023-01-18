bf4f0d11d3f6cd78de83da9b3b40a588a7615f57
Young il Cho <0young@kakao.com>
2023-01-18 16:26:13 +0900

    feat: redux-logger 추가

--- src/util/redux/index.ts
+++ src/util/redux/index.ts
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from 'redux-logger'
+
 import page from "./page";
 import comments from "./comments";
 import form from "./form";
@@ -9,6 +11,7 @@ const store = configureStore({
     comments,
     form,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
